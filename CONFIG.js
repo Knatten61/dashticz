c64b0e37498502f24b0926a5e29edfe9a7794c0a
Knatten61 <71945694+Knatten61@users.noreply.github.com>
2020-09-26 22:14:19 +0200

    Create CONFIG.js

---
+++ CONFIG.js
@@ -0,0 +1,22 @@
+var config = {}                                                                                                
+config['domoticz_ip'] = 'http://192.168.200.13:8080';                                                          
+config['app_title'] = 'Dashticz V3';                                                                           
+config['domoticz_refresh'] = '5';                                                                              
+config['dashticz_refresh'] = '60';                                                                             
+                                                                                                               
+config['use_favorites'] = 0; //Request all Domoticz Devices, not only favorites                                
+config['auto_positioning'] = 0; // Use 0 this if you have defined your own columns                             
+                                                                                                               
+//Definition of blocks                                                                                         
+blocks = {}                                                                                                    
+blocks[93] = { width: 6}                                                                                       
+blocks[94] = { width: 6}                                                                                       
+                                                                                                               
+//Definition of columns                                                                                        
+columns = {}                                                                                                   
+columns[1] = {width: 4}
+columns[2] = { blocks : [94, 93], width: 8}                                                                    
+                                                                                                               
+//Definition of screens                                                                                        
+screens = {}                                                                                                   
+screens[1] = {columns: [1, 2]}                                                                       
