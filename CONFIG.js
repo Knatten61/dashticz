cd1e05ac4666b2de813281b759547963d249c8d6
Knatten61 <71945694+Knatten61@users.noreply.github.com>
2020-09-27 08:18:19 +0200

    Update CONFIG.js

--- CONFIG.js
+++ CONFIG.js
@@ -1,6 +1,6 @@
 var config = {}                                                                                                
 config['domoticz_ip'] = 'http://192.168.200.13:8080';                                                          
-config['app_title'] = 'Dashticz V3';                                                                           
+config['app_title'] = 'Knatten kontrollpanel';                                                                           
 config['domoticz_refresh'] = '5';                                                                              
 config['dashticz_refresh'] = '60';                                                                             
                                                                                                                
@@ -13,17 +13,18 @@ config['default_news_url'] = 'https://www.nrk.no/toppsaker.rss';
 blocks = {}                                                                                                    
 blocks[93] = { width: 6}                                                                                       
 blocks[94] = { width: 6}                                                                                       
+blocks[49] = { width: 4}
+blocks[33] = { width: 4}                                                                                       
+
                                                                                                                
 //Definition of columns                                                                                        
 columns = {}                                                                                                   
-//columns[1] = {width: 4}
-columns[2] = { blocks : [94, 93], width: 8}                                                                    
-
-columns[1] = {
-  blocks: ['news'],
-  width: 4
-}
+columns[1] = { blocks : [94, 93], width: 8}                                                                    
+columns[2] = {blocks: ['news'],width: 4}
+columns[3] = {blocks: [49,33], width:4}
                                                                                                                
 //Definition of screens                                                                                        
 screens = {}                                                                                                   
-screens[1] = {columns: [1, 2]}                                                                       
+screens[1] = {columns: [1, 2]}  
+screens[2] = {columns: [3]}  
+
